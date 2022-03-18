336e3e5cdf7cb358ebd9dabb67ee9d6a5a6a81ac
Nikolay0811 <n.s.d_bg@abv.bg>
2022-03-17 21:56:36 -1000

    commit2

---
+++ Chushka12D/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Chushka12D.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Chushka12D.Areas.Identity.IdentityHostingStartup))]
+namespace Chushka12D.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
